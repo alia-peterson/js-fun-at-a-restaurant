78b4a202f917deb88bcf2fce44e02bc6acadac07
Alia Peterson <alia@pnwebs.com>
2020-10-07 20:09:34 -0600

    Finished meal exercise

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,43 @@
-function nameMenuItem() {
-
+function nameMenuItem(nameInput) {
+  return `Delicious ${nameInput}`
 }
 
+function createMenuItem(newMenuItemName, newMenuItemPrice, newMenuItemType) {
+  return {
+    name: newMenuItemName,
+    price: newMenuItemPrice,
+    type: newMenuItemType
+  }
+}
+
+function addIngredients(newIngredientName, arrayName) {
+  if (arrayName.includes(newIngredientName) === false) {
+    arrayName.push(newIngredientName)
+  }
+  return arrayName
+}
+
+function formatPrice(newMenuItemPrice) {
+  return `$${newMenuItemPrice}`
+}
+
+function decreasePrice(newMenuItemPrice) {
+  return newMenuItemPrice * 0.9
+}
+
+function createRecipe(newRecipeTitle, newRecipeIngredients, newRecipeType) {
+  return {
+    title: newRecipeTitle,
+    ingredients: newRecipeIngredients,
+    type: newRecipeType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
