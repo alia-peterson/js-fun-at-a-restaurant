9b56db2ef50c55e6da08cb0bbc33463c7941516f
Alia Peterson <alia@pnwebs.com>
2020-10-07 21:24:50 -0600

    Finished order exercises

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(newOrder, orderArray) {
+  if (orderArray.length < 3) {
+    orderArray.push(newOrder)
+  }
+  return orderArray
+}
+
+function refundOrder(orderToRefund, orderArray) {
+  return orderArray = orderArray.splice(orderToRefund - 1, 1)
+}
+
+function listItems(orderArray) {
+  return orderArray.map(eachOrder => eachOrder.item).join(', ')
+}
+// we were told not to use arrow functions but i figure since this is
+// part of the map definition, it would be okay?
+// the alternative would be a for loop which would look like this:
+/*
+let orderArrayItems = []
+for (let i = 0; i < orderArray.length; i++) {
+  orderArrayItems.push(orderArray[i].item)
+}
+return orderArrayItems.join(', ')
+*/
+
+function searchOrder(orderArray, orderItem) {
+  let orderIncludesItem = false
+  for (var i = 0; i < orderArray.length; i++) {
+    if (orderArray[i].item.includes(orderItem)) {
+      orderIncludesItem = true
+    }
+  }
+  return orderIncludesItem
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
