cb07414dab0c74a359a3ecdb87387f1ed35e4a25
Alia Peterson <alia@pnwebs.com>
2020-10-11 12:53:15 -0600

    Finished chef exercise

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,21 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurantName) {
+    this.name = chefName;
+    this.restaurant = restaurantName
+  }
+  greetCustomer(customerName, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${customerName}!`
+    }
+    return `Hello, ${customerName}!`
+  }
+
+  checkForFood(requestedFood) {
+    if (this.restaurant.menus[requestedFood.type].includes(requestedFood)) {
+      return `Yes, we're serving ${requestedFood.name} today!`
+    }
+    return `Sorry, we aren't serving ${requestedFood.name} today.`
   }
 }
 
-module.exports = Chef;
+module.exports = Chef
